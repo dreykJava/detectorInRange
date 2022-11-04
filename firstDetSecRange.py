3024d0e497bfff9a60651b4a489225a337a6b05e
FizMaks <maks.valiaev@yandex.ru>
2022-11-04 16:34:59 +0700

    fixed recognition method and add frame resize

--- firstDetSecRange.py
+++ firstDetSecRange.py
@@ -51,7 +51,7 @@ def getSignCoordinates(img) -> list:
     
     for contour in contours2:
         x, y, x2, y2 = [contour.left(), contour.top(), contour.right(), contour.bottom()]
-        cv.rectangle(img, (x, y), (x2, y2), (0, 0, 255), 3)
+        cv.rectangle(img, (x, y), (x2, y2), (0, 150, 0), 3)
         imgesList.append(img[y : y2, x : x2])
 
     return imgesList
@@ -65,16 +65,16 @@ def getMask(img_rgb, color, red = 0):
             mask_down = cv.inRange(img, (0, color[0][1], color[0][2]), (color[2], color[1][1], color[1][2]))
             mask = cv.bitwise_or(mask, mask_down)
 
-        cv.imshow('test', mask)
+        # cv.imshow('test', mask)
 
         mask = cv.dilate(mask, np.ones((3, 3)))
         mask = cv.erode(mask, np.ones((3, 3)))
 
-        cv.imshow('test1', mask)
+        # cv.imshow('test1', mask)
 
         return cv.resize(mask, (64, 64))
     except:
-        print(type(img_rgb))
+        # print(type(img_rgb))
         return np.zeros((64, 64))
 
 etalon_park_mask = getMask(etalon_park, blue)
@@ -98,44 +98,52 @@ def compare(mask, color):
         # print("098", etalon_stop_mask.shape)
         # print()
 
-        if mask.shape:
-            print("YES")
-        else:
-            print("NONONONONONONONNONON")
+        # if mask.shape:
+        #     print("YES")
+        # else:
+        #     print("NONONONONONONONNONON")
 
-        stop_comp[mask == etalon_stop_mask] = 1
+        stop_comp[mask == etalon_stop_mask] = 1 # много матриц это не хорошо, цените память, молодой человек!
         krug_comp[mask == etalon_krug_mask] = 1
 
-        if np.sum(krug_comp) >= 2700 and np.sum(krug_comp) != 3552:
-            print(np.sum(krug_comp))
-            time.sleep(1)
-            print('Знак проезда нет')
-        elif np.sum(stop_comp) >= 2700:
-            print(np.sum(stop_comp))
-            time.sleep(1)
-            print('Знак стоп')
+        # теперь будет выводиться не первый похожий, а наиболее похожий
+        comp = [('Знак проезда нет', np.sum(krug_comp)), ('Знак стоп', np.sum(stop_comp))]
+        comp = sorted(comp, key = lambda x: x[1], reverse=True)
+        if comp[0][1] > 2700:
+            print(comp[0][0])
+
+        
+
+        # if np.sum(krug_comp) >= 2700 and np.sum(krug_comp) != 3552:
+        #     print(np.sum(krug_comp))
+        #     time.sleep(1)
+        #     print('Знак проезда нет')
+        # elif np.sum(stop_comp) >= 2700:
+        #     print(np.sum(stop_comp))
+        #     time.sleep(1)
+        #     print('Знак стоп')
             
 
 capture = cv.VideoCapture(0)
 
 while True:
     ret, frame_rgb = capture.read()
-
+    frame_rgb = cv.resize(frame_rgb, (frame_rgb.shape[1] // 2, frame_rgb.shape[0] // 2))
     if frame_rgb.any():
         frame_list = getSignCoordinates(frame_rgb)
 
         for frame in frame_list:
             frameBlue = getMask(frame, blue)
-            cv.imshow("blue", cv.resize(frameBlue, (128, 128)))
+            # cv.imshow("blue", cv.resize(frameBlue, (128, 128)))
 
             frameRed = getMask(frame, red, 1)
-            cv.imshow("red", cv.resize(frameRed, (128, 128)))
+            # cv.imshow("red", cv.resize(frameRed, (128, 128)))
 
             compare(frameBlue, blue)
             compare(frameRed, red)
 
         cv.imshow("frame", frame_rgb)
-
+        
     if (cv.waitKey(30)) == 27:
         break
 
